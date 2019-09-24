60dd41681dd798ffc7bbda0069da6784e164fbef
AlisonYoon <politescarlett1@gmail.com>
2019-09-24 15:41:21 +0100

    This is how Factory looks like

--- src/dependencies.php
+++ src/dependencies.php
@@ -2,13 +2,13 @@
 
 use Slim\App;
 
-return function (App $app) {
+return function (App $app) {    //this is a function Factory
     $container = $app->getContainer();
 
     // view renderer
     $container['renderer'] = function ($c) {
         $settings = $c->get('settings')['renderer'];
-        return new \Slim\Views\PhpRenderer($settings['template_path']);
+        return new \Slim\Views\PhpRenderer($settings['template_path']); //It's instantiating an object and pass something in it
     };
 
     // monolog
@@ -16,7 +16,7 @@ return function (App $app) {
         $settings = $c->get('settings')['logger'];
         $logger = new \Monolog\Logger($settings['name']);
         $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
-        $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+        $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));    //Lots of passing-in happening here.
         return $logger;
     };
 
